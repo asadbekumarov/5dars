2f0304350d0ecbf00f036ebc7c7cefec4fc7a7e2
asadbekumarov <asadbekumarov922@gmail.com>
2024-06-30 03:57:33 +0500

    5_dars

---
+++ JS/script.js
@@ -0,0 +1,91 @@
+const elInput = document.querySelector("input");
+const elBtn = document.querySelector("#btn");
+const elResult = document.querySelector("#result");
+const elResult2 = document.querySelector("#result2");
+const elResult3 = document.querySelector("#result3");
+const elResult4 = document.querySelector("#result4");
+
+
+
+const personSpeed = 3.6;
+const bycucleSpeed = 20.1;
+const carSpeed = 70;
+const palneSpeed = 800;
+
+elBtn.addEventListener("click", function () {
+  const space = elInput.value;
+
+  const time = space / personSpeed;
+
+  const hour = Math.floor(time);
+
+  const allMinutes = (time - hour) * 60;
+
+  const minutes = Math.floor(allMinutes);
+  
+  const seconds = Math.round((allMinutes - minutes) * 60 )
+
+  elResult.textContent = `${hour} soat ${minutes} daqiqa ${seconds} soniya`;
+
+  // bycucleSpeed/////////////////////
+
+  const space2 = elInput.value;
+
+  const time2 = space2 / bycucleSpeed;
+
+  const hour2 = Math.floor(time2);
+
+  const allMinutes2 = (time2 - hour2) * 60;
+
+  const minutes2 = Math.floor(allMinutes2);
+
+  const seconds2 = Math.round((allMinutes - minutes) * 60 )
+
+  elResult2.textContent = `${hour2} soat ${minutes2} daqiqa ${seconds} soniya`;
+
+// carSpeed/////////////////////////
+
+const space3 = elInput.value;
+
+const time3 = space3 / carSpeed;
+
+const hour3 = Math.floor(time3);
+
+const allMinutes3 = (time3 - hour3) * 60;
+
+const minutes3 = Math.floor(allMinutes3);
+
+const seconds3 = Math.round((allMinutes - minutes) * 60 )
+
+elResult3.textContent = `${hour3} soat ${minutes3} daqiqa ${seconds} soniya`;
+
+// palneSpeed///////////////////////
+
+const space4 = elInput.value;
+
+const time4 = space4 / palneSpeed;
+
+const hour4 = Math.floor(time4);
+
+const allMinutes4 = (time4 - hour4) * 60;
+
+const minutes4 = Math.floor(allMinutes4);
+
+const seconds4 = Math.round((allMinutes - minutes) * 60 )
+
+elResult4.textContent = `${hour4} soat ${minutes4} daqiqa ${seconds} soniya`;
+});
+// DARK MODE////////////////////////
+
+// const btn = document.querySelector('button')
+// btn.addEventListener('click',() =>{
+// document.body.classList.add(tun)
+// document.body.classList.toggle('Tun')
+// }) 
+
+
+let modemoon = document.getElementById("light-dark");
+
+modemoon.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
